792444a15a0d1560a367902d96499452ec87b28e
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-12-30 19:28:13 +0200

    Update baze.cpp

--- pirmauzduotis0.11-1.01/baze.cpp
+++ pirmauzduotis0.11-1.01/baze.cpp
@@ -1,1 +1,46 @@
 
+#include "baze.h"
+baze::baze(const baze& studentas):
+
+	vardas{ studentas.vardas },
+	pavarde{ studentas.pavarde },
+		vid{ studentas.vid },
+		paz{ studentas.paz },
+		kiekis{ studentas.kiekis },
+		egz{ studentas.egz },
+		galutinismed{ studentas.galutinismed },
+		galutinisvid{ studentas.galutinisvid },
+		sum{ studentas.sum },
+		med{ studentas.med }
+	 {
+	vardas = studentas.vardas;
+	pavarde = studentas.pavarde;
+	vid = studentas.vid;
+	paz = studentas.paz;
+	egz = studentas.egz;
+	galutinismed = studentas.galutinismed;
+	galutinisvid = studentas.galutinisvid;
+	sum = studentas.sum;
+	med = studentas.med;
+	 }
+
+
+
+baze& baze::operator=(const baze& studentas) {
+
+    if (&studentas == this) return *this;
+
+	paz.clear();
+	vardas = studentas.vardas;
+	pavarde = studentas.pavarde;
+	vid = studentas.vid;
+	paz = studentas.paz;
+	egz = studentas.egz;
+	galutinismed = studentas.galutinismed;
+	galutinisvid = studentas.galutinisvid;
+	sum = studentas.sum;
+	med = studentas.med;
+	return *this;
+
+
+}
