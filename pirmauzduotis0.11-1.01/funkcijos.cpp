581137beba48aa0f9f8152be345863455a3bee23
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-12-30 19:01:40 +0200

    Update funkcijos.cpp

--- pirmauzduotis0.11-1.01/funkcijos.cpp
+++ pirmauzduotis0.11-1.01/funkcijos.cpp
@@ -7,6 +7,7 @@ void ketvirta(int irasusk) {
 	int nd;
 	srand(time(NULL));
 	vector <baze> studentai;
+	baze studentas;
 	cout << "iveskite namu darbu skaiciu " << endl;
 	cin >> nd;
 	skaicius_ir_tikr(nd);
@@ -15,17 +16,17 @@ void ketvirta(int irasusk) {
 		r = rand() % 10;
 		t = rand() % 10;
 		baze b;
-		b.vardas = name[r] + to_string(rand() % 100 + 1);
-		b.pavarde = pavarde[t] + to_string(rand() % 100 + 1);
-		b.kiekis = nd;
+		studentas.setvardas(name[r] + to_string(rand() % 100 + 1));
+		studentas.setpavarde(pavarde[t] + to_string(rand() % 100 + 1));
+		studentas.setkiekis(nd);
 		vector <int> paz;
 
 		for (int j = 0; j < nd; j++) {
 			k = rand() % 10 + 1;
 			paz.push_back(k);
 		}
-		b.paz = paz;
-		b.egz = rand() % 10 + 1;
+		studentas.setpaz(paz);
+		studentas.setegz(rand() % 10 + 1);
 		studentai.push_back(b);
 
 	}
@@ -34,9 +35,9 @@ void ketvirta(int irasusk) {
 	for (int j = 0; j < nd; j++) { rf << left << setw(15) << setfill(' ') << "ND" << j + 1; }
 	rf << left << setw(15) << setfill(' ') << "egzaminas" << endl;
 	for (auto& a : studentai) {
-		rf << left << setw(15) << setfill(' ') << a.vardas << left << setw(15) << setfill(' ') << a.pavarde;
-		for (auto& aa : a.paz) { rf << left << setw(15) << setfill(' ') << aa; }
-		rf << left << setw(15) << setfill(' ') << a.egz << endl;
+		rf << left << setw(15) << setfill(' ') << a.getvardas() << left << setw(15) << setfill(' ') << a.getpavarde();
+		for (auto& aa : a.getpaz()) { rf << left << setw(15) << setfill(' ') << aa; }
+		rf << left << setw(15) << setfill(' ') << a.getegz() << endl;
 	}
 	rf.close();
 }
@@ -49,21 +50,29 @@ void nuskaitymas(string failas, vector <baze>& studentai) {
 	g = count(antraste.begin(), antraste.end(), 'N');
 	while (!df.eof()) {
 		baze b;
-		b.kiekis = g;
+		b.setkiekis(g);
 		n = n + 1;
-		b.sum = 0;
-		df >> b.vardas >> b.pavarde;
-		for (int i = 0; i < b.kiekis; i++) {
-			float sk;
-			df >> sk; b.paz.push_back(sk); b.sum = b.sum + sk;
-		}
-		b.vid = b.sum / b.kiekis;
+		float sum = 0;
+		string vardas, pavarde;
 
-		sort(b.paz.begin(), b.paz.end());
-		if (b.kiekis % 2 == 1) b.med = b.paz.at((b.kiekis + 1) / 2); else b.med = b.paz.at(b.kiekis / 2) + b.paz.at(((b.kiekis / 2) + 1) / 2);
-		df >> b.egz;
-		b.galutinisvid = 0.4 * b.vid + 0.6 * b.egz;
-		b.galutinismed = 0.4 * b.med + 0.6 * b.egz;
+		df >>vardas >> pavarde;
+		b.setvardas(vardas);
+		b.setpavarde(pavarde);
+		vector <int> paz;
+		for (int i = 0; i < b.getkiekis(); i++) {
+			float sk;
+			df >> sk; paz.push_back(sk); sum = sum + sk;
+		}
+		b.setpaz(paz);
+		b.setsum(sum);
+	
+		sort(b.getpaz().begin(), b.getpaz().end());
+		if (b.getkiekis() % 2 == 1) b.setmed( b.getpaz().at((b.getkiekis() + 1) / 2)); else b.setmed( b.getpaz().at(b.getkiekis() / 2) + b.getpaz().at(((b.getkiekis() / 2) + 1) / 2));
+		float egz;
+		df >> egz;
+		b.setegz(egz);
+		b.setgalutinisvid();
+		b.setgalutinismed();
 		studentai.push_back(b);
 		studentai.reserve(n);
 	}
@@ -76,14 +85,14 @@ void funkcija(string failas, vector <baze> studentai) {
 	rf << left << setw(15) << setfill(' ') << "vardas" << left << setw(15) << setfill(' ') << "pavarde" << left << setw(15) << setfill(' ') << "galutinisvid" << left << setw(15) << setfill(' ') << "galutinismed" << endl;
 	rf << string(60, '-') << endl;
 	for (auto& a : studentai) {
-		rf << left << setw(15) << setfill(' ') << a.vardas << left << setw(15) << setfill(' ') << a.pavarde << left << setw(15) << setfill(' ') << fixed << setprecision(2) << a.galutinisvid << left << setw(15) << setfill(' ') << fixed << setprecision(2) << a.galutinismed << endl;
+		rf << left << setw(15) << setfill(' ') << a.getvardas() << left << setw(15) << setfill(' ') << a.getpavarde() << left << setw(15) << setfill(' ') << fixed << setprecision(2) << a.getgalutinisvid() << left << setw(15) << setfill(' ') << fixed << setprecision(2) << a.getgalutinismed() << endl;
 	}
 
 	rf.close();
 }
 void pirma(vector <baze> studentai, vector <baze> &vargsiukai, vector <baze> &galvociai) {
 	for (auto& a : studentai) {
-		if (a.galutinisvid >= 5) galvociai.push_back(a);
+		if (a.getgalutinisvid() >= 5) galvociai.push_back(a);
 		else vargsiukai.push_back(a);
 
 	}
@@ -94,10 +103,10 @@ void padalinimostrategija(vector <baze>& studentai, vector <baze>& vargsiukai) {
 
     for (auto& a : studentai) {
      
-		if (a.galutinisvid < 5)  vargsiukai.push_back(a); 		 
+		if (a.getgalutinisvid() < 5)  vargsiukai.push_back(a); 		 
 }
 
 	studentai.erase(remove_if(studentai.begin(), studentai.end(), [&](baze
-		const& studentas) { return studentas.galutinisvid < 5; }), studentai.end());
+		const& studentas) { return studentas.getgalutinisvid() < 5; }), studentai.end());
 }
 
