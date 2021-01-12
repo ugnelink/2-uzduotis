d1332058a88fd546d893360b95785abe8ad97433
ugnelink <71488162+ugnelink@users.noreply.github.com>
2021-01-12 19:00:23 +0200

    Update trycatch.h

--- pirmauzduotis0.11-1.01/trycatch.h
+++ pirmauzduotis0.11-1.01/trycatch.h
@@ -1,5 +1,6 @@
 
-#pragma once
+#ifndef TRYCATCH_H
+#define TRYCATCH_H
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -7,3 +8,4 @@ using namespace std;
 void iv_ir_tikr(int& a);
 void paz_ir_tikr(float& a);
 void skaicius_ir_tikr(int& a);
+#endif
